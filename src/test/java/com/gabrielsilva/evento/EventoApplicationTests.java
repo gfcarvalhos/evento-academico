63d19fceae66bc3a88d4835a79e99034e746ff74
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2026-09-13 13:13:55 -0300

    first commit

---
+++ src/test/java/com/gabrielsilva/evento/EventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielsilva.evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
