5b46b626c8c1e94a1e7762db993f758d3edcef47
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2026-09-13 19:34:12 -0300

    criacao da entidade Participante

---
+++ src/main/java/com/gabrielsilva/evento/entities/Participante.java
@@ -0,0 +1,48 @@
+package com.gabrielsilva.evento.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    public Participante() {}
+
+    public Participante(Long id, String email, String name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
