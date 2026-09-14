36847b80e6ecfb23711fe4e3e6367bc87aba6814
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2026-09-13 22:34:10 -0300

    finalizacao da entidades e relacionamentos

--- src/main/java/com/gabrielsilva/evento/entities/Participante.java
+++ src/main/java/com/gabrielsilva/evento/entities/Participante.java
@@ -2,31 +2,39 @@ package com.gabrielsilva.evento.entities;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 @Entity
 @Table(name = "tb_participante")
 public class Participante {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String name;
 
     @Column(unique = true)
     private String email;
 
+    @ManyToMany(mappedBy = "participantes")
+    private Set<Atividade> atividades = new HashSet<>();
+
     public Participante() {}
 
-    public Participante(Long id, String email, String name) {
+    public Participante(Integer id, String email, String name, Set<Atividade> atividades) {
         this.id = id;
         this.email = email;
         this.name = name;
+        this.atividades = atividades;
     }
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -44,5 +52,25 @@ public class Participante {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Set<Atividade> getAtividades() {
+        return atividades;
+    }
+
+    public void setAtividades(Set<Atividade> atividades) {
+        this.atividades = atividades;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Participante that = (Participante) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
